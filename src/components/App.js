50867c1658da335ddfe9b667fba89c5bbc754267
AyushAvi <70689163+AyushAvi@users.noreply.github.com>
2022-04-24 02:44:23 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/App.css';
 import { Preview } from './Preview';
 const App = () => {
 
+  const [content, setContent] = useState('Newton School');
+  const [fontSize, setFontSize] = useState(10);
+  const [padding, setPadding] = useState(5);
+
+  const handleContent = (e) =>{
+    setContent(e.target.value)
+  }
+
+  const handleFontSize = (e) => {
+    setFontSize(e.target.value);
+  }
+
+  const handlePadding = (e) =>{
+    setPadding(e.target.value)
+  }
+
+  const obj = {
+    fontSize : fontSize,
+    content : content,
+    padding : padding
+  }
+
   return (
     <div id="main">
       <div>
-        {/* add input elememnts inside here */}
+        <input type="text" onChange={handleContent} id='contentInput' value={content}/>
+        <input type="number" onChange={handleFontSize} value={fontSize} id="fontSizeInput"/>
+        <input type="number" onChange={handlePadding} value={padding} id='paddingInput'/>
       </div>
  
- {/* render Preview component here */}
+ <Preview {...obj}/>
  
     </div>
   )
